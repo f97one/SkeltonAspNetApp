b063fb09b9b7f69819873d6371a8c5f14c7891ac
HAJIME Fukuna <f97one@gmail.com>
2018-04-28 06:36:02 +0900

    プロジェクト ファイルを追加します。

---
+++ SkeltonAspNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkeltonAspNetApp.Startup))]
+namespace SkeltonAspNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
