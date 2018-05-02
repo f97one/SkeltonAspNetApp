ea969b1b499c1baf1a3b4f53d047720e2a407ca1
HAJIME Fukuna <f97one@gmail.com>
2018-05-02 16:43:22 +0900

    Entity Framework Code First 初期化処理を追加。

---
+++ SkeltonAspNetApp/Repositories/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SkeltonAspNetApp.Repositories
+{
+    /// <summary>
+    /// Entity Framework Code First のDbContext
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        /// <summary>
+        /// 接続文字列の名前を引数にとるコンストラクタ.
+        /// </summary>
+        public　AppDbContext() : base("DefaultContext") { }
+
+        // TODO: DbSet のプロパティを定義する
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ SkeltonAspNetApp/Repositories/AppDbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SkeltonAspNetApp.Repositories
+{
+    public class AppDbInitializer : DropCreateDatabaseIfModelChanges<AppDbContext>
+    {
+        protected override void Seed(AppDbContext context)
+        {
+            // TODO : マイグレーション時に投入する初期データの処理を書く
+        }
+    }
+}
